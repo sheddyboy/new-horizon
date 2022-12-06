83033f7998760ab650ebc0adb1b94508db1a905a
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-12-06 17:36:35 +0100

    feat: init mqtt

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/App.module.scss";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Wrapper from "./mqtt_wrapper";
 
 function App() {
+  const [searchParams] = useSearchParams();
+  const [mqttData] = useState({
+    mqttServer: searchParams.get("mqtt_server"),
+    sessionId: searchParams.get("session_id"),
+    user: searchParams.get("user"),
+    password: searchParams.get("password"),
+  });
   const navigate = useNavigate();
   useEffect(() => {
-    // Wrapper.connect("", "", "", "");
+    Wrapper.connect(
+      mqttData.mqttServer ? mqttData.mqttServer : "",
+      mqttData.sessionId ? mqttData.sessionId : "",
+      mqttData.user ? mqttData.user : "",
+      mqttData.password ? mqttData.password : ""
+    );
 
-    return () => {
-      // Wrapper.send("connected", 0);
-    };
-  }, []);
+    // return () => {
+    //   Wrapper.send("connected", 0);
+    // };
+  }, [mqttData]);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.backgroungImage}></div>
       <div className={styles.top}>
         <img src="/LOGO.svg" alt="logo" />
-        <h1>Create Your Own Horizon</h1>
+        <h1>Create Your New Horizon</h1>
       </div>
       <div className={styles.bottom}>
         <button
