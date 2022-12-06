2ede1e3806b5cc1c4628df13d612c092bd28c2bb
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-12-06 11:21:09 +0100

    style: Add padding bottom 20px

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles/App.module.scss";
 import { useNavigate } from "react-router-dom";
+import Wrapper from "./mqtt_wrapper";
 
 function App() {
   const navigate = useNavigate();
+  useEffect(() => {
+    // Wrapper.connect("", "", "", "");
+
+    return () => {
+      // Wrapper.send("connected", 0);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.backgroungImage}></div>
