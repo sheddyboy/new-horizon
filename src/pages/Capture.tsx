55a5f3c9d17887cd4e539477b0d3838e60a584d8
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-12-09 21:45:23 +0100

    feat: Create capture page

---
+++ src/pages/Capture.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CreateCtx } from "../context/CreateProvider";
+import Wrapper from "../mqtt_wrapper";
+import styles from "../styles/Capture.module.scss";
+
+const Capture = () => {
+  const { height } = useContext(CreateCtx);
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className={styles.wrapper}
+      style={{ height: height < 670 ? "100vh" : "", transition: "all 0.5s" }}
+    >
+      <h1>Now stand in front of the projector</h1>
+      <button
+        onClick={() => {
+          try {
+            Wrapper.send("Capture", "");
+          } catch (err) {
+            console.log(err);
+          }
+          navigate("/camera");
+        }}
+      >
+        Capture
+      </button>
+    </div>
+  );
+};
+
+export default Capture;
